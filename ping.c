505df49b7dfadf55300ba06486779a73f77fc410
Tomato <Tomato.HF@gmail.com>
2015-03-08 23:15:24 +0800

    check args

--- ping.c
+++ ping.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 int main(int argc, char **argv) {
+    if (argc != 2) {
+        printf("usage: %s host\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
     return EXIT_SUCCESS;
 }
